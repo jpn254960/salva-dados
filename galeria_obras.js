1ee4b3312e44fc11800bf93be302da62ef15d605
kj <111208020+jpn254960@users.noreply.github.com>
2025-01-03 23:08:09 -0300

    dd
    dd

--- galeria_obras.js
+++ galeria_obras.js
@@ -32,3 +32,18 @@ function passar(s){
         document.getElementById('text_numeral').innerText=`${sequencia+1}/${fotos.length}`
     }
 }
+
+function abrir(){
+    var barra=document.getElementById('barra')
+    var verificador= barra.className
+    if(verificador=='barra' || verificador=='barra_0'){
+        barra.className='barra_1'
+        barra.style.transform='translateX(0%)'
+        document.getElementById('bt_barra').style.color='white'   
+    }
+    else if (verificador=='barra_1'){
+            barra.className='barra_0'
+            barra.style.transform='translateX(100%)'
+        document.getElementById('bt_barra').style.color='black'   
+    }
+}
